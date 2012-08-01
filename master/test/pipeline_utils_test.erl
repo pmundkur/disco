b28e1541d4fa088d7a55567b68eb250b25625730
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-07-31 18:04:30 -0700

    Fix the test by generating only valid pipelines.

--- master/test/pipeline_utils_test.erl
+++ master/test/pipeline_utils_test.erl
@@ -1,4 +1,5 @@
 -module(pipeline_utils_test).
+-compile(export_all).
 
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
@@ -7,34 +8,44 @@
 -include("disco.hrl").
 -include("pipeline.hrl").
 
-pipe_graph(Pipeline) ->
-    pipe_graph(digraph:new([acyclic]), pipeline_utils:stages(Pipeline)).
+pipe_graph_new(Pipeline) ->
+    pipe_graph(digraph:new([acyclic]), Pipeline).
 pipe_graph(G, []) ->
     G;
-pipe_graph(G, [H | []]) ->
-    HV = digraph:add_vertex(G, H),
-    TV = digraph:add_vertex(G, done),
-    digraph:add_edge(G, HV, TV),
+pipe_graph(G, [{H, _HG} | []]) ->
+    _HV = digraph:add_vertex(G, H),
     G;
-pipe_graph(G, [H | [T|_] = Rest]) ->
+pipe_graph(G, [{H, _HG} | [{T, TG} | _] = Rest]) ->
     HV = digraph:add_vertex(G, H),
     TV = digraph:add_vertex(G, T),
-    digraph:add_edge(G, HV, TV),
+    digraph:add_edge(G, HV, TV, TG),
     pipe_graph(G, Rest).
 
+pipe_graph_delete(G) -> digraph:delete(G).
+
 pg_next_stage(G, V) ->
-    case digraph:out_neighbours(G, V) of
-        [N] -> N;
-        _   -> false
+    case digraph:out_edges(G, V) of
+        [] ->
+            done;
+        [NE | []] ->
+            {NE, V, N, NG} = digraph:edge(G, NE),
+            {N, NG}
     end.
 
 prop_next_stage() ->
-    ?FORALL(Pipeline, pipeline(),
-            ?FORALL(Stage, union(pipeline_utils:stages(Pipeline)),
+    ?FORALL(P, ?SUCHTHAT(P, pipeline(),
+                         begin
+                             S = pipeline_utils:stages(P),
+                             P =/= [] andalso
+                                 gb_sets:size(gb_sets:from_list(S)) =:= length(P)
+                         end),
+            ?FORALL(Stage, union(pipeline_utils:stages(P)),
                     begin
-                        PG = pipe_graph(Pipeline),
-                        pipeline_utils:next_stage(Pipeline, Stage)
-                            =:= pg_next_stage(PG, Stage)
+                        PG = pipe_graph_new(P),
+                        Check = (pipeline_utils:next_stage(P, Stage)
+                                 =:= pg_next_stage(PG, Stage)),
+                        pipe_graph_delete(PG),
+                        Check
                     end)).
 
 do_test() ->
