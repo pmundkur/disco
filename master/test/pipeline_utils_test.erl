91d7bd59d840cee31d402f6af007c7c434fdacc1
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2012-08-01 17:13:28 -0700

    Add a simple property test for grouping.

--- master/test/pipeline_utils_test.erl
+++ master/test/pipeline_utils_test.erl
@@ -48,9 +48,24 @@ prop_next_stage() ->
                         Check
                     end)).
 
+useful_input({data, {_L, _S, []}}) -> false;
+useful_input({dir, {_H, _U, []}})  -> false;
+useful_input(_) -> true.
+
+prop_distinct_groups() ->
+    ?FORALL({LG, TOs}, {label_grouping(), [{task_id(), [task_output()]}]},
+            begin
+                GOs = pipeline_utils:group_outputs(LG, TOs),
+                Groups = [G || {G, _O} <- GOs],
+                UOs = [O || {Tid, Tout} <- TOs, {Outid, O} <- Tout, useful_input(O)],
+                % split generates one output per useful input
+                (LG =:= split andalso length(UOs) =:= length(GOs))
+                    orelse (length(Groups) =:= gb_sets:size(gb_sets:from_list(Groups)))
+            end).
+
 do_test() ->
     EunitLeader = erlang:group_leader(),
     erlang:group_leader(whereis(user), self()),
-    Res = proper:module(?MODULE),
+    Res = proper:module(?MODULE, [{numtests, 200}]),
     erlang:group_leader(EunitLeader, self()),
     ?assertEqual([], Res).
